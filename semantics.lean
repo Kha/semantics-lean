3a9ad692a3e45f2db62a0d3dc4cceb627906d26d
Sebastian Ullrich <sebasti@nullri.ch>
2015-05-05 21:04:24 +0200

    attribution, structure, more notation

--- semantics.lean
+++ semantics.lean
@@ -1,11 +1,37 @@
-import data.int.basic
-import data.int.order
+/- Formulization of the operational semantics of a simple While language
+   Based on the lecture notes of 'Semantics of programming languages', KIT, 2015
+   https://pp.info.uni-karlsruhe.de/lehre/SS2015/semantik/
+-/
+
+import data.int
 
 open bool
 open int
 open eq.ops
 
--- 1
+-- should probably become part of the standard library at some point?
+
+definition bool.of_decidable (p : Prop) [dec : decidable p] : bool :=
+match dec with
+| inl := tt
+| inr := ff
+end
+
+definition string.decidable_eq [instance] (s₁ s₂ : string) : decidable (s₁ = s₂) := sorry
+
+namespace function
+  variables {A B : Type}
+  variables [dec : Π(a a' : A), decidable (a = a')]
+  include dec
+
+  definition update (f : A → B) (a : A) (b : B) : A → B := λa', if a' = a then b else f a'
+
+  notation f `[` a `↦` b `]` := update f a b
+end function
+
+open function
+
+-- section 2: definition of While
 
 abbreviation Var := string
 notation `Σ`     := Var → ℤ
@@ -18,19 +44,22 @@ inductive AExp :=
 
 namespace AExp
   attribute lit [coercion]
-  attribute var [coercion]
 
-  infixl `-` := sub
-  prefix `-` := λx, 0 - x
-  infixl `+` := λx y, x - (-y)
-  infixl `*` := mul
+  -- should coercions resolve abbreviations?
+  definition var' : string → AExp := var
+  attribute var' [coercion]
 
-  definition val (σ : Σ): AExp → ℤ
+  infixl `-`     := sub
+  notation - x   := 0 - x
+  notation x + y := x - (-y)
+  infixl `*`     := mul
+
+  definition val (σ : Σ) : AExp → ℤ
   | (lit n) := n
   | (var x) := σ x
-  -- no infix?
-  | (sub a b) := val a - val b
-  | (mul a b) := val a * val b
+  -- namespace needed?
+  | (#AExp a - b) := val a - val b
+  | (#AExp a * b) := val a * val b
 
   notation `A⟦` a `⟧` σ:100 := val σ a
 
@@ -48,23 +77,16 @@ inductive BExp :=
 | not : BExp → BExp
 | and : BExp → BExp → BExp
 
-definition bool.of_eq (p : Prop) [dec : decidable p] : bool :=
-match dec with
-| inl := tt
-| inr := ff
-end
-
 namespace BExp
-  infixl `≤` := leq
+  infixl `≤`  := leq
   infixl `&&` := and
-
-  prefix `¬` := bnot
+  prefix `¬`  := bnot
 
   definition val (σ : Σ) : BExp → bool
-  | true := tt
-  | (leq a₁ a₂) := bool.of_eq (le A⟦a₁⟧σ A⟦a₂⟧σ)
-  | (not b) := ¬val b
-  | (b₁ && b₂) := val b₁ && val b₂
+  | true        := tt
+  | (leq a₁ a₂) := bool.of_decidable (A⟦a₁⟧σ < A⟦a₂⟧σ)
+  | (not b)     := ¬val b
+  | (b₁ && b₂)  := val b₁ && val b₂
 
   notation `B⟦` b `⟧` σ:100 := val σ b
 end BExp
@@ -83,19 +105,19 @@ open Com
 infix `::=`:50 := ass
 infix `;;`:30  := seq
 
--- 3
+example: Com := "x"::=1;; while ("x" ≤ 4) ("x" ::= "x" * 2)
 
-definition string.eq.decidable [instance] (s₁ s₂ : string) : decidable (s₁ = s₂) := sorry
+-- chapter 3.1: a bigstep semantics of While
 
 inductive bigstep : Com → Σ → Σ → Prop :=
-| skip : Π{σ : Σ}, bigstep skip σ σ
-| ass  : Π{x : Var} {a : AExp} {σ : Σ}, bigstep (x ::= a) σ
-  (λx', if x' = x then A⟦a⟧σ else σ x')
-| seq  : Π{c₁ c₂ : Com} {σ σ' σ'': Σ} (H₁ : bigstep c₁ σ σ') (H₂ : bigstep c₂ σ' σ''), bigstep (c₁;;c₂) σ σ''
-| if_tt : Π{b : BExp} {c₁ c₂ : Com} {σ σ': Σ} (Hb : B⟦b⟧σ = tt) (H₁ : bigstep c₁ σ σ'), bigstep (cond b c₁ c₂) σ σ'
-| if_ff : Π{b : BExp} {c₁ c₂ : Com} {σ σ': Σ} (Hb : B⟦b⟧σ = ff) (H₂ : bigstep c₂ σ σ'), bigstep (cond b c₁ c₂) σ σ'
-| while_tt : Π{b : BExp} {c : Com} {σ σ' σ'': Σ} (Hb : B⟦b⟧σ = tt) (Hc : bigstep c σ σ') (Hind : bigstep (while b c) σ' σ''), bigstep (while b c) σ σ''
-| while_ff : Π{b : BExp} {c : Com} {σ: Σ} (Hb : B⟦b⟧σ = ff), bigstep (while b c) σ σ
+  notation `⟨` c `,` σ `⟩⟱` σ':100 := bigstep c σ σ'
+| skip : Π{σ}, ⟨skip,σ⟩⟱σ
+| ass  : Π{x a σ}, ⟨x ::= a,σ⟩⟱σ[x ↦ A⟦a⟧σ]
+| seq  : Π{c₁ c₂ σ σ' σ''} (H₁ : ⟨c₁,σ⟩⟱σ') (H₂ : ⟨c₂,σ'⟩⟱σ''), ⟨c₁;;c₂,σ⟩⟱σ''
+| if_tt : Π{b c₁ c₂ σ σ'} (Hb : B⟦b⟧σ = tt) (H₁ : ⟨c₁,σ⟩⟱σ'), ⟨cond b c₁ c₂,σ⟩⟱σ'
+| if_ff : Π{b c₁ c₂ σ σ'} (Hb : B⟦b⟧σ = ff) (H₂ : ⟨c₂,σ⟩⟱σ'), ⟨cond b c₁ c₂,σ⟩⟱σ'
+| while_tt : Π{b c σ σ' σ''} (Hb : B⟦b⟧σ = tt) (Hc : ⟨c,σ⟩⟱σ') (Hind : ⟨while b c,σ'⟩⟱σ''), ⟨while b c,σ⟩⟱σ''
+| while_ff : Π{b c σ} (Hb : B⟦b⟧σ = ff), ⟨while b c,σ⟩⟱σ
 
 notation `⟨` c `,` σ `⟩⟱` σ':100 := bigstep c σ σ'
 
@@ -103,25 +125,26 @@ namespace bigstep
   variables {b : BExp} {c : Com}
   variables {σ σ' σ₁ σ₂ : Σ}
 
-  lemma unroll_loop' : ⟨while b c, σ⟩⟱σ' ↔ ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ' :=
-    iff.intro
-      (assume H : ⟨while b c, σ⟩⟱σ',
-       show ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
-       begin
-         cases H,
-         { apply (bigstep.if_tt Hb (bigstep.seq Hc Hind)) },
-         { apply (bigstep.if_ff Hb bigstep.skip) }
-       end)
-      (assume H : ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
-       show ⟨while b c, σ⟩⟱σ',
-       begin
-         cases H,
-         { cases H₁, apply (bigstep.while_tt Hb H₁_1 H₂) },
-         { cases H₂, apply (bigstep.while_ff Hb) }
-       end)
+  lemma unroll_loop : ⟨while b c, σ⟩⟱σ' ↔ ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ' :=
+  iff.intro
+    (assume H : ⟨while b c, σ⟩⟱σ',
+     show ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
+     begin
+       cases H,
+       { apply (bigstep.if_tt Hb (bigstep.seq Hc Hind)) },
+       { apply (bigstep.if_ff Hb bigstep.skip) }
+     end)
+    (assume H : ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
+     show ⟨while b c, σ⟩⟱σ',
+     begin
+       cases H,
+       { cases H₁, apply (bigstep.while_tt Hb H₁_1 H₂) },
+       { cases H₂, apply (bigstep.while_ff Hb) }
+     end)
 
   lemma deterministic (H₁ : ⟨c, σ⟩⟱σ₁) (H₂ : ⟨c, σ⟩⟱σ₂) : σ₁ = σ₂ :=
   begin
+    -- induction on H₁ for the predicate '∀σ₂, ⟨c, σ⟩⟱σ₂ → σ₁ = σ₂'
     revert H₂,
     revert σ₂,
     eapply bigstep.induction_on H₁,
@@ -138,7 +161,7 @@ namespace bigstep
       intro b c₁ c₂ σ σ' Hb Hc₁ Hind σ'₂ H₂,
       cases H₂,
       { exact Hind σ'_1 H₁ },
-      { have Hcontr : ff = tt, from (Hb_1⁻¹ ⬝ Hb), contradiction }
+      { exact bool.no_confusion (Hb_1⁻¹ ⬝ Hb) }
     },
     { clear H₁ c σ σ₁,
       intro b c₁ c₂ σ σ' Hb Hc₁ Hind σ'₂ H₂,

f67edfc346f763083db35483a6960a9c1e5e3be0
Sebastian Ullrich <sebasti@nullri.ch>
2017-04-05 10:53:20 +0200

    Port to Lean 3 and simplify

--- semantics.lean
+++ semantics.lean
@@ -3,63 +3,40 @@
    https://pp.info.uni-karlsruhe.de/lehre/SS2015/semantik/
 -/
 
-import data.int
-
-open bool
-open int
-open eq.ops
-
--- should probably become part of the standard library at some point?
-
-definition bool.from_decidable (p : Prop) [dec : decidable p] : bool :=
-match dec with
-| inl := tt
-| inr := ff
-end
-
-definition string.decidable_eq [instance] (s₁ s₂ : string) : decidable (s₁ = s₂) := sorry
-
 namespace function
-  variables {A B : Type}
-  variables [dec : Π(a a' : A), decidable (a = a')]
-  include dec
+  variables {α β : Type}
+  variables [decidable_eq α]
 
-  definition update (f : A → B) (a : A) (b : B) : A → B := λa', if a' = a then b else f a'
+  def update (f : α → β) (a : α) (b : β) : α → β :=
+  λ a', if a' = a then b else f a'
 
-  notation f `[` a `↦` b `]` := update f a b
+  notation f `[` a ` ↦ ` b `]` := update f a b
 end function
-
-open function
 
 -- section 2: definition of While
 
-abbreviation Var := string
-notation `Σ`     := Var → ℤ
+notation `Var` := string
+notation `Σ` := Var → ℤ
 
-inductive AExp :=
+inductive AExp
 | lit : ℤ → AExp
 | var : Var → AExp
 | sub : AExp → AExp → AExp
 | mul : AExp → AExp → AExp
 
 namespace AExp
-  attribute lit [coercion]
+  instance has_coe_from_int : has_coe ℤ AExp := ⟨lit⟩
+  instance has_coe_from_Var : has_coe Var AExp := ⟨var⟩
+  instance : has_sub AExp := ⟨sub⟩
+  instance : has_mul AExp := ⟨mul⟩
+  instance : has_neg AExp := ⟨λ x, ↑0 - x⟩
+  instance : has_add AExp := ⟨λ x y, x - -y⟩
 
-  -- should coercions resolve abbreviations?
-  definition var' : string → AExp := var
-  attribute var' [coercion]
-
-  infixl `-`     := sub
-  notation - x   := 0 - x
-  notation x + y := x - (-y)
-  infixl `*`     := mul
-
-  definition val (σ : Σ) : AExp → ℤ
+  def val (σ : Σ) : AExp → ℤ
   | (lit n) := n
   | (var x) := σ x
-  -- namespace needed?
-  | (#AExp a - b) := val a - val b
-  | (#AExp a * b) := val a * val b
+  | (sub a b) := val a - val b
+  | (mul a b) := val a * val b
 
   notation `A⟦` a `⟧` σ:100 := val σ a
 
@@ -69,31 +46,27 @@ namespace AExp
             ... = A⟦a₁⟧σ + A⟦a₂⟧σ       : by rewrite [zero_sub, sub_neg_eq_add]
 end AExp
 
-open AExp
-
-inductive BExp :=
+inductive BExp
 | true : BExp
 | leq : AExp → AExp → BExp
 | not : BExp → BExp
 | and : BExp → BExp → BExp
 
 namespace BExp
-  infixl `≤`  := leq
-  infixl `&&` := and
-  prefix `¬`  := bnot
+  infixl ≤  := leq
+  infixl && := and
+  prefix ¬  := bnot
 
   definition val (σ : Σ) : BExp → bool
   | true        := tt
-  | (leq a₁ a₂) := bool.from_decidable (A⟦a₁⟧σ ≤ A⟦a₂⟧σ)
+  | (leq a₁ a₂) := A⟦a₁⟧σ ≤ A⟦a₂⟧σ
   | (not b)     := ¬val b
-  | (b₁ && b₂)  := val b₁ && val b₂
+  | (and b₁ b₂) := val b₁ && val b₂
 
   notation `B⟦` b `⟧` σ:100 := val σ b
 end BExp
 
-open BExp
-
-inductive Com :=
+inductive Com
 | skip : Com
 | ass : Var → AExp → Com
 | seq : Com → Com → Com
@@ -102,22 +75,22 @@ inductive Com :=
 
 open Com
 
-infix `::=`:50 := ass
-infix `;;`:30  := seq
+infix ` ::= `:50 := ass
+infix `;; `:30  := seq
 
-example: Com := "x"::=1;; while ("x" ≤ 4) ("x" ::= "x" * 2)
+example: Com := "x" ::= ↑1;; while ("x" ≤ ↑4) ("x" ::= "x" * ↑2)
 
 -- chapter 3.1: a bigstep semantics of While
 
-inductive bigstep : Com → Σ → Σ → Prop :=
+inductive bigstep : Com → Σ → Σ → Prop
   notation `⟨` c `,` σ `⟩⟱` σ':100 := bigstep c σ σ'
-| skip : Π{σ}, ⟨skip,σ⟩⟱σ
-| ass  : Π{x a σ}, ⟨x ::= a,σ⟩⟱σ[x ↦ A⟦a⟧σ]
-| seq  : Π{c₁ c₂ σ σ' σ''} (H₁ : ⟨c₁,σ⟩⟱σ') (H₂ : ⟨c₂,σ'⟩⟱σ''), ⟨c₁;;c₂,σ⟩⟱σ''
-| if_tt : Π{b c₁ c₂ σ σ'} (Hb : B⟦b⟧σ = tt) (H₁ : ⟨c₁,σ⟩⟱σ'), ⟨cond b c₁ c₂,σ⟩⟱σ'
-| if_ff : Π{b c₁ c₂ σ σ'} (Hb : B⟦b⟧σ = ff) (H₂ : ⟨c₂,σ⟩⟱σ'), ⟨cond b c₁ c₂,σ⟩⟱σ'
-| while_tt : Π{b c σ σ' σ''} (Hb : B⟦b⟧σ = tt) (Hc : ⟨c,σ⟩⟱σ') (Hind : ⟨while b c,σ'⟩⟱σ''), ⟨while b c,σ⟩⟱σ''
-| while_ff : Π{b c σ} (Hb : B⟦b⟧σ = ff), ⟨while b c,σ⟩⟱σ
+| skip {σ} : ⟨skip,σ⟩⟱σ
+| ass  {x a σ} : ⟨x ::= a,σ⟩⟱σ[x ↦ A⟦a⟧σ]
+| seq  {c c' σ σ' σ''} (h : ⟨c,σ⟩⟱σ') (h' : ⟨c',σ'⟩⟱σ'') : ⟨c;;c',σ⟩⟱σ''
+| if_tt {b cₜ cₑ σ σ'} (hb : B⟦b⟧σ = tt) (ht : ⟨cₜ,σ⟩⟱σ') : ⟨cond b cₜ cₑ,σ⟩⟱σ'
+| if_ff {b cₜ cₑ σ σ'} (hb : B⟦b⟧σ = ff) (he : ⟨cₑ,σ⟩⟱σ') : ⟨cond b cₜ cₑ,σ⟩⟱σ'
+| while_tt {b c σ σ' σ''} (hb : B⟦b⟧σ = tt) (hc : ⟨c,σ⟩⟱σ') (hind : ⟨while b c,σ'⟩⟱σ'') : ⟨while b c,σ⟩⟱σ''
+| while_ff {b c σ} (hb : B⟦b⟧σ = ff) : ⟨while b c,σ⟩⟱σ
 
 notation `⟨` c `,` σ `⟩⟱` σ':100 := bigstep c σ σ'
 
@@ -127,53 +100,53 @@ namespace bigstep
 
   lemma unroll_loop : ⟨while b c, σ⟩⟱σ' ↔ ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ' :=
   iff.intro
-    (assume H : ⟨while b c, σ⟩⟱σ',
+    (assume h : ⟨while b c, σ⟩⟱σ',
      show ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
      begin
-       cases H,
-       { apply (bigstep.if_tt Hb (bigstep.seq Hc Hind)) },
-       { apply (bigstep.if_ff Hb bigstep.skip) }
+       cases h,
+       { apply bigstep.if_tt hb (bigstep.seq hc hind) },
+       { apply bigstep.if_ff hb bigstep.skip }
      end)
-    (assume H : ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
+    (assume h : ⟨cond b (c;; while b c) Com.skip, σ⟩⟱σ',
      show ⟨while b c, σ⟩⟱σ',
      begin
-       cases H,
-       { cases H₁, apply (bigstep.while_tt Hb H₁_1 H₂) },
-       { cases H₂, apply (bigstep.while_ff Hb) }
+       cases h,
+       { cases ht, apply bigstep.while_tt hb h_1 h' },
+       { cases he, apply bigstep.while_ff hb }
      end)
 
-  lemma deterministic (H₁ : ⟨c, σ⟩⟱σ₁) (H₂ : ⟨c, σ⟩⟱σ₂) : σ₁ = σ₂ :=
+  lemma deterministic (h₁ : ⟨c, σ⟩⟱σ₁) (h₂ : ⟨c, σ⟩⟱σ₂) : σ₁ = σ₂ :=
   begin
-    -- induction on H₁ for the predicate '∀σ₂, ⟨c, σ⟩⟱σ₂ → σ₁ = σ₂'
-    revert H₂,
+    -- induction on h₁ for the predicate '∀ σ₂, ⟨c, σ⟩⟱σ₂ → σ₁ = σ₂'
+    revert h₂,
     revert σ₂,
-    induction H₁,
-    { intro σ₂ H₂, cases H₂, exact rfl },
-    { intro σ₂ H₂, cases H₂, exact rfl },
-    { intro σ₂ H₂',
-      cases H₂',
-      apply v_1,
-      rewrite (v_0 σ'_1 H₁_1),
-      exact H₂_1
+    induction h₁,
+    { intros σ₂ h₂, cases h₂, exact rfl },
+    { intros σ₂ h₂, cases h₂, exact rfl },
+    { intros σ₂ h₂,
+      cases h₂,
+      apply ih_2,
+      rewrite ih_1 h_1,
+      assumption
     },
-    { intro σ₂ H₂,
-      cases H₂,
-      { exact v_0 σ₂ H₁_1 },
-      { exact bool.no_confusion (Hb_1⁻¹ ⬝ Hb) }
+    { intros σ₂ h₂,
+      cases h₂,
+      { exact ih_1 hₜ_1 },
+      { rewrite hb_1 at hb, contradiction }
     },
-    { intro σ₂ H₂,
-      cases H₂,
-      { exact bool.no_confusion (Hb_1⁻¹ ⬝ Hb) },
-      { exact v_0 σ₂ H₂_1 }
+    { intros σ₂ h₂,
+      cases h₂,
+      { rewrite hb_1 at hb, contradiction },
+      { exact ih_1 hₑ_1 }
     },
-    { intro σ₂ H₂,
-      cases H₂,
-      { apply v_1, rewrite (v_0 σ'_1 Hc_1), exact Hind_1 },
-      { exact bool.no_confusion (Hb_1⁻¹ ⬝ Hb) }
+    { intros σ₂ h₂,
+      cases h₂,
+      { apply ih_2, rewrite ih_1 hc_1, assumption },
+      { rewrite hb_1 at hb, contradiction }
     },
-    { intro σ₂ H₂,
-      cases H₂,
-      { exact bool.no_confusion (Hb_1⁻¹ ⬝ Hb) },
+    { intros σ₂ h₂,
+      cases h₂,
+      { rewrite hb_1 at hb, contradiction },
       { exact rfl }
     }
   end
